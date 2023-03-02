d7a008306cace7aa4bc810ddeb98e2121dfc192c
mehedi9868 <mehedi9868@gmail.com>
2023-03-03 05:10:28 +0600

    dummy modal added

--- js/app.js
+++ js/app.js
@@ -40,8 +40,12 @@ const showTools = (receivedData, dataLimit) => {
                 <h2 class="card-title mb-3">${singleTool.name}</h2>
                 <p>${singleTool.published_in}</p>
             </div>
-            <button class="bg-red-50 hover:bg-red-100  px-4 py-3 text-center items-center rounded-[50%]"><i
-                    class="fa-solid fa-arrow-right text-red-400"></i></button>
+            <button class="" onclick="fetchToolsDetails('${singleTool.id}')">
+                <label for="modal">
+                    <i class="fa-solid fa-arrow-right text-red-400 bg-red-50 hover:bg-red-100  px-4 py-4 text-center items-center rounded-[50%]"></i>
+                </label>
+            </button>
+            
         </div>
     </div>`
 
@@ -58,7 +62,87 @@ document.getElementById('btn-see-more').addEventListener('click', function () {
     fetchAllTools();
 });
 
+// for modal:
+// load dynamic API:
+const fetchToolsDetails = (id) => {
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showToolsDetails(data))
+}
 
+// show modal:
+
+const showToolsDetails = dataDetails => {
+    console.log(dataDetails.data);
+    const { features, integrations, image_link, tool_name } = dataDetails.data;
+    document.getElementById('modal-body').innerHTML =
+        `
+        <div class="modal-box relative">
+            <label for="modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+            <div class="flex justify-between gap-6 p-6">
+                <div id="left-section" class="bg-green-100 w-full">
+                    <h2>${tool_name}</h2>
+                    <div class="flex justify-between">
+                        <h2>1</h2>
+                        <h2>2</h2>
+                        <h2>3</h2>
+                    </div>
+                    <div class="flex justify-between">
+                        <div>
+                            <h2></h2>
+                            <ul class="list-decimal list-inside">
+                                <li>${features[1].feature_name}</li>
+                                <li>${features[2].feature_name}</li>
+                                <li>${features[3].feature_name}</li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h2></h2>
+                            <ul class="list-decimal list-inside">
+                                <li>${integrations[0]}</li>
+                                <li>${integrations[1]}</li>
+                                <li>${integrations[2]}</li>   
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div id="right-section" class="bg-red-100 w-full">
+                    <img src="${image_link[0]}" alt="">
+                    <h2>text</h2>
+                    <p>paragraph</p>
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const showNewsDetails = newsDetails => {
+//     const { title, image_url, details, others_info } = newsDetails;
+//     document.getElementById('exampleModalLabel').innerText = title;
+//     document.getElementById('modal-body').innerHTML = `
+//     <span class="badge text-bg-danger">${others_info.is_trending ? "Trending" : ""}</span>
+//     <img src="${image_url}" class="img-fluid mt-1" alt="...">
+//         <div>
+//             <p class="card-text mt-2">${details}</p>
+//         </div>
+//     `;
+// }
 
 
 
