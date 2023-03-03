7c63e9601619d9d02ee020884f7dde2a2f2ac7a5
mehedi9868 <mehedi9868@gmail.com>
2023-03-04 00:50:51 +0600

    updates error data handling

--- js/app.js
+++ js/app.js
@@ -34,9 +34,9 @@ const showTools = (receivedData, dataLimit) => {
         <div class="card-body">
             <h2 class="card-title">Features</h2>
             <ol class="list-decimal list-inside">
-                <li>${singleTool && singleTool.features[0] ? singleTool.features[0] : 'No Data Found'}</li>
-                <li>${singleTool && singleTool.features[1] ? singleTool.features[1] : 'No Data Found'}</li>
-                <li>${singleTool && singleTool.features[2] ? singleTool.features[2] : 'No Data Found'}</li>
+                <li>${singleTool && singleTool?.features[0] ? singleTool.features[0] : 'No Data Found'}</li>
+                <li>${singleTool && singleTool?.features[1] ? singleTool.features[1] : 'No Data Found'}</li>
+                <li>${singleTool && singleTool?.features[2] ? singleTool.features[2] : 'No Data Found'}</li>
             </ol>
         <hr class="my-4">
         <div class="flex justify-between items-center">
@@ -72,6 +72,7 @@ document.getElementById('btn-see-more').addEventListener('click', function () {
 // load dynamic API:
 const fetchToolsDetails = (id) => {
     const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+    console.log(url);
     fetch(url)
         .then(res => res.json())
         .then(data => showToolsDetails(data))
@@ -91,18 +92,19 @@ const showToolsDetails = dataDetails => {
                 <div id="left-section" class="bg-red-50 w-full border-2 border-red-400 rounded-lg p-6">
                     <h2 class="font-bold mb-5">${description}</h2>
                     <div class="flex flex-col sm:flex-row sm:justify-between gap-3">
-                        <h2 class="bg-slate-50 p-4 sm:p-7 rounded-xl text-green-600 font-bold text-center sm:text-left">${pricing && pricing[0] ? pricing[0].price : 'Free of Cost'} <br/> ${pricing[0].plan}
+                        <h2 class="bg-slate-50 p-4 sm:p-7 rounded-xl text-green-600 font-bold text-center sm:text-left">${pricing && pricing[0]?.price ? pricing[0].price : 'Free of Cost'} <br/> ${pricing && pricing[0]?.plan ? pricing[0].plan : 'Basic'}
                         </h2>
-                        <h2 class="bg-slate-50 p-4 sm:p-7 rounded-xl text-orange-400 font-bold text-center sm:text-left">${pricing && pricing[1] ? pricing[1].price : 'Free of Cost'} <br/> ${pricing[1].plan}</h2>
-                        <h2 class="bg-slate-50 p-4 sm:p-7 rounded-xl text-red-600 font-bold text-center sm:text-left">${pricing && pricing[2] ? pricing[2].price : 'Free of Cost'} <br/> ${pricing[2].plan}</h2>
+                        <h2 class="bg-slate-50 p-4 sm:p-7 rounded-xl text-orange-400 font-bold text-center sm:text-left">${pricing && pricing[1]?.price ? pricing[1].price : 'Free of Cost'} <br/> ${pricing && pricing[1]?.plan ? pricing[1].plan : 'Pro'}</h2>
+                        <h2 class="bg-slate-50 p-4 sm:p-7 rounded-xl text-red-600 font-bold text-center sm:text-left">${pricing && pricing[2]?.price ? pricing[2].price : 'Free of Cost'} <br/> ${pricing && pricing[2]?.plan ? pricing[2].plan : 'Enterprise'}</h2>
                     </div>
                     <div class="flex justify-between">
                         <div>
                             <h2 class="font-bold my-5">Features</h2>
                             <ul class="list-disc list-inside">
-                                <li>${features && features[1] ? features[1].feature_name : 'No Data Found'}</li>
-                                <li>${features && features[2] ? features[2].feature_name : 'No Data Found'}</li>
-                                <li>${features && features[3] ? features[3].feature_name : 'No Data Found'}</li>
+                                <li>${features && features[1]?.feature_name ? features[1].feature_name : 'No Data Found'}</li>
+                                <li>${features && features[2]?.feature_name ? features[2].feature_name : 'No Data Found'}</li>
+                                <li>${features && features[3]?.feature_name ? features[3].feature_name : 'No Data Found'}</li>
+                                <li>${features && features[4]?.feature_name ? features[4].feature_name : 'No Data Found'}</li>
                             </ul>
                         </div>
                         <div>
@@ -111,6 +113,8 @@ const showToolsDetails = dataDetails => {
                                 <li>${integrations && integrations[0] ? integrations[0] : 'No Data Found'}</li>
                                 <li>${integrations && integrations[1] ? integrations[1] : 'No Data Found'}</li>
                                 <li>${integrations && integrations[2] ? integrations[2] : 'No Data Found'}</li>   
+                                <li>${integrations && integrations[3] ? integrations[3] : 'No Data Found'}</li>   
+                                <li>${integrations && integrations[4] ? integrations[4] : 'No Data Found'}</li>   
                             </ul>
                         </div>
                     </div>
@@ -121,8 +125,8 @@ const showToolsDetails = dataDetails => {
                         <p class="absolute top-2 right-2 bg-red-600 font-bold text-white px-8 py-2 rounded-lg ${accuracy && accuracy.score ? '' : 'hidden'}">
                         ${accuracy && accuracy.score ? accuracy.score * 100 + '% accuracy' : 'No Data Found'}</p>
                     </div>
-                    <h2 class="text-center font-bold mt-5 mb-3">${input_output_examples && input_output_examples[0] ? input_output_examples[0].input : 'No Data Found'}</h2>
-                    <p class="text-center mb-3">${input_output_examples && input_output_examples[0] ? input_output_examples[0].output : 'No Data Found'}</p>
+                    <h2 class="text-center font-bold mt-5 mb-3">${input_output_examples && input_output_examples[0]?.input ? input_output_examples[0].input : 'Can you give any example?'}</h2>
+                    <p class="text-center mb-3">${input_output_examples && input_output_examples[0]?.output ? input_output_examples[0].output : 'No! Not Yet! Take a break!!!'}</p>
                 </div>
             </div>
         </div>
